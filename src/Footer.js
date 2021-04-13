8b770c962a7afaeb27070df309b13734c08e751f
Arwinder  Singh <30037045@student.toiohomai.ac.nz>
2021-04-13 15:52:07 +1200

    GH-page-commit

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Footer extends Component
+{
+    render()
+    {
+        return(
+            <footer className="text-muted py-5">
+  <div className="container">
+    <p className="float-end mb-1">
+      
+    </p>
+    <p className="mb-1">Name: Arwinder Singh</p>
+    <p className="mb-0">Student Id:30037045 </p>
+  </div>
+</footer>
+        );
+    }
+}
+
+export default Footer;
